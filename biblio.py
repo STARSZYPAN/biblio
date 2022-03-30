c5541cb828bb8adf767a9ce7a49d19582a5965ac
STARSZYPAN <98351502+STARSZYPAN@users.noreply.github.com>
2022-03-30 19:34:35 +0200

    first commit

---
+++ biblio.py
@@ -0,0 +1,47 @@
+
+
+import random
+
+
+class Film:
+    def __init__(self,tytuł: str,rok_wyd: int ,gatunek: str):
+        self.tytuł=tytuł
+        self.rok_wyd=rok_wyd
+        self.gatunek=gatunek
+        self.counter=0
+
+    def __str__(self):
+        return f"{self.tytuł} {self.rok_wyd} {self.gatunek}"
+
+    def play(self):
+        self.counter +=1
+
+class Serial(Film):
+    def __init__(self,sezon: int,epizod: int,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.sezon=sezon
+        self.epizod=epizod
+
+    def __str__(self):
+        return f"{self.tytuł} {self.rok_wyd} {self.gatunek} S{self.sezon} E{self.epizod}"
+
+
+baza=[Film("Venom 2", 2021 ,"Sci-fi"),Film("Project Adam",2022,"Sci-fi"),Film("Monthy Python i Święty Grall",1975,"Komedia"),
+    Serial("Peaky Blinder's",2018,"Sensacyjny" ,1,2),Serial("Vikings",2018,"Przygodowy",2,3)]
+
+def genarate_views(baza :Film):
+    for i in range(random.randint(0,10)):
+        baza.play()
+
+def get_movies(baza: list):
+    result = []
+    for f in baza:
+        if isinstance(f,Film) and not  isinstance (f,Serial):
+            result.append(f)
+        return result
+
+for f in baza:
+    print (f,Counter)
+
+print (get_movies(baza))
+
